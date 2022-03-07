ec0a57b0c7c9c801c233b47e2638c4d143279e77
Vijay Shiyani <vshiyani@meeco.me>
2022-03-07 18:27:55 +1100

    final vc sdk api

--- demo/1_issue_credential.js
+++ demo/1_issue_credential.js
@@ -1,6 +1,6 @@
 const { PrivateKey, Client } = require("@hashgraph/sdk");
-const { HcsDid } = require("../dist");
-const { OPERATOR_ID, OPERATOR_KEY } = require("./.env.json");
+const { HcsVc } = require("../dist");
+const { OPERATOR_ID, OPERATOR_KEY, TOPIC_ID } = require("./.env.json");
 
 async function main() {
     /**
@@ -9,12 +9,67 @@ async function main() {
     const client = Client.forTestnet();
     client.setOperator(OPERATOR_ID, OPERATOR_KEY);
 
+    const issuerPrivateKey = PrivateKey.generate();
+    const Issuer = new Issuer(privateKey: issuerPrivateKey, did: issuerDID, name: name );
+ 
     /**
-     * Build DID instance
+     *  Issue cred
+     * 
      */
-    const didPrivateKey = PrivateKey.generate();
-    const did = new HcsDid({ privateKey: didPrivateKey, client: client });
-    const registeredDid = await did.register();
+     const claims = {
+        "name": "Melbourne Uni",
+        "address": "Melbourne",
+        "code": "12345",
+        "course": {
+            "id": "122345",
+            "courseCode": "ws101",
+            "name": "WorkSafe Basic Training Module",
+            "version": "2021-01-01",
+            "provider": "WorkPro Training"
+        }
+    }
+
+    const vc = new HcsVc({ issuer: _issuer, subjectDID: subjectDID, client: client, type: "cert ", claims: claims, expiryDate: _date });
+
+   
+
+    // create cred - singed by issuer pk 
+    // create hash 
+    // create message 
+    //{
+    //     "mode": "plain",
+    //     "message": {
+    //         "operation": "issue",
+    //         "credentialHash": "BSgRkqRSCZwZJrgWtbsTvZG7s93zBYYxP2Dtqwm25ajb",
+    //         "timestamp": "2022-03-04T02:47:18.192323Z"
+    //     },
+    //     "signature": "uw1Bl7ARRm27ZjhG+9aYRJ+N6zmzvZOI10koz6T6gSS5GJAci9OTnYyL2LVJgGilx1hAHCL2A3nkQ6AU6eNPCw=="
+    // }
+    // submit transaction
+    // output : W3C Cred signed by issuer and subject as did -  "credentialHash": "7L6ZqXZzusWvMfzRCTrzjan2AgPFotQzfWqdzXwVNHkV"
+
+
+    const registeredDid = await vc.issue();
+
+    //get status - ACTIVE, REVOKE, SU.....
+    const status = await vc.verifyStatus();
+
+
+    // find out vc hash
+    // create message
+    // create message 
+    //{
+    //     "mode": "plain",
+    //     "message": {
+    //         "operation": "revoke",
+    //         "credentialHash": "BSgRkqRSCZwZJrgWtbsTvZG7s93zBYYxP2Dtqwm25ajb",
+    //         "timestamp": "2022-03-04T02:47:18.192323Z"
+    //     },
+    //     "signature": "uw1Bl7ARRm27ZjhG+9aYRJ+N6zmzvZOI10koz6T6gSS5GJAci9OTnYyL2LVJgGilx1hAHCL2A3nkQ6AU6eNPCw=="
+    // }
+    // submit transaction
+    // status
+    const revoke = await vc.revoke();
 
     console.log("\n");
     console.log(`DID PRIVATE KEY: ${didPrivateKey.toString()}`);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import { VCError, VCErrorCode } from "./identity/vc-error";
+
+import { HcsVc } from "./identity/hcs/vc/hcs-vc";
+import { HcsVcEventMessageResolver } from "./identity/hcs/vc/hcs-vc-event-message-resolver";
+import { HcsVcMessage } from "./identity/hcs/vc/hcs-vc-message";
+import { HcsVcTopicListener } from "./identity/hcs/vc/hcs-vc-topic-listener";
+import { HcsVcTransaction } from "./identity/hcs/vc/hcs-vc-transaction";
 import { JsonClass } from "./identity/hcs/json-class";
 import { MessageEnvelope } from "./identity/hcs/message-envelope";
 import { SerializableMirrorConsensusResponse } from "./identity/hcs/serializable-mirror-consensus-response";
@@ -6,11 +13,20 @@ import { Ed25519PubCodec } from "./utils/ed25519PubCodec";
 import { Hashing } from "./utils/hashing";
 import { TimestampUtils } from "./utils/timestamp-utils";
 import { Validator } from "./utils/validator";
+import { VcMethodOperation } from "./identity/ vc-method-operation";
 
 export {
     ArraysUtils,
+    VCError,
+    VCErrorCode,
+    VcMethodOperation,
     Ed25519PubCodec,
     Hashing,
+    HcsVc,
+    HcsVcEventMessageResolver,
+    HcsVcMessage,
+    HcsVcTopicListener,
+    HcsVcTransaction,
     JsonClass,
     MessageEnvelope,
     SerializableMirrorConsensusResponse,
