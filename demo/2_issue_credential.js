48e24be586910df3ee6c2ab87124d24bd6f85b90
Linas Išganaitis <l.isganaitis@gmail.com>
2022-03-09 16:08:58 +0000

    HcsVc API changes to handle revocation list

--- demo/2_issue_credential.js
+++ demo/2_issue_credential.js
@@ -1,9 +1,19 @@
-const { PrivateKey, Client } = require("@hashgraph/sdk");
+const { PrivateKey, Client, FileId } = require("@hashgraph/sdk");
 const { HcsVc } = require("../dist");
-const { OPERATOR_ID, OPERATOR_KEY, SUBJECT_DID, ISSUER_DID, ISSUER_PK, TOPIC_ID } = require("./.env.json");
+const {
+    OPERATOR_ID,
+    OPERATOR_KEY,
+    SUBJECT_DID,
+    REVOCATION_LIST_FILE_ID,
+    ISSUER_DID,
+    ISSUER_PK,
+    TOPIC_ID,
+} = require("./.env.json");
 var fs = require("fs");
 
 const rl = require("vc-revocation-list");
+
+const revocationListFileId = FileId.fromString(REVOCATION_LIST_FILE_ID);
 
 async function main() {
     /**
@@ -35,34 +45,38 @@ async function main() {
     /**
      *  Issue cred
      */
-    let vc = await hcsVc.issue({
-        credentialSubject: {
-            id: SUBJECT_DID,
-            givenName: "Jane",
-            familyName: "Doe",
-            degree: {
-                type: "BachelorDegree",
-                name: "Bachelor of Science and Arts",
-                college: "College of Engineering",
+    let vc = await hcsVc.issue(
+        {
+            credentialSubject: {
+                id: SUBJECT_DID,
+                givenName: "Jane",
+                familyName: "Doe",
+                degree: {
+                    type: "BachelorDegree",
+                    name: "Bachelor of Science and Arts",
+                    college: "College of Engineering",
+                },
             },
-        },
-        credentialSchema: {
-            id: "https://example.org/examples/degree.json",
-            type: "JsonSchemaValidator2018",
-        },
-        expiration: new Date("2022-01-01T00:00:00Z"),
-        evidence: [
-            {
-                id: "https://example.edu/evidence/f2aeec97-fc0d-42bf-8ca7-0548192d4231",
-                type: ["DocumentVerification"],
-                verifier: "https://example.edu/issuers/14",
-                evidenceDocument: "DriversLicense",
-                subjectPresence: "Physical",
-                documentPresence: "Physical",
-                licenseNumber: "123AB4567",
+            credentialSchema: {
+                id: "https://example.org/examples/degree.json",
+                type: "JsonSchemaValidator2018",
             },
-        ],
-    });
+            expiration: new Date("2022-01-01T00:00:00Z"),
+            evidence: [
+                {
+                    id: "https://example.edu/evidence/f2aeec97-fc0d-42bf-8ca7-0548192d4231",
+                    type: ["DocumentVerification"],
+                    verifier: "https://example.edu/issuers/14",
+                    evidenceDocument: "DriversLicense",
+                    subjectPresence: "Physical",
+                    documentPresence: "Physical",
+                    licenseNumber: "123AB4567",
+                },
+            ],
+        },
+        revocationListFileId,
+        0
+    );
 
     // create cred - singed by issuer pk
     // create hash
