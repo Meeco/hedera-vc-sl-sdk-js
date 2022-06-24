801a91f9d0850cca9b6c77e845ca33234976e04b
Vijay Shiyani <vshiyani@meeco.me>
2022-06-24 11:06:45 +1000

    Squashed commit of the following:
    commit ab735a39a7b1c43e24cd0b7bf42a718b36d73387
    Author: Vijay Shiyani <vshiyani@meeco.me>
    Date:   Fri Jun 24 11:02:40 2022 +1000
    
        removing op-key and id
    
    commit ed59d464f312c96fed4c36b598249c2c6af554e9
    Author: Vijay Shiyani <vshiyani@meeco.me>
    Date:   Fri Jun 24 11:01:22 2022 +1000
    
        updated hashgarph sdk js to stable version with latest patch release only

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,3 @@
-process.env.OPERATOR_ID = "0.0.55..";
-process.env.OPERATOR_KEY = "302e02010..";
+// process.env.OPERATOR_ID = "0.0.12710..";
+// process.env.OPERATOR_KEY =
+//     "302e020100300506..";
